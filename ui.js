34b9799902b0f7bef21a5c4e770f95cf97cf92e4
Jason Jalufka <jasonjalufka@gmail.com>
2016-08-28 08:52:33 -0500

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){//Initial text fade-in
+$(document).ready(function(){
     alert("Hello! I am an alert box!");
+    
+    //Initial text fade-in
+    $('.fade-in').each(function() {
+    $(this).fadeIn(1000).delay(1000);
+    });
 });
