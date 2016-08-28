75b255afb9b46408038efaab32793376caca7e11
jasonjalufka <jasonjalufka@gmail.com>
2016-08-28 00:38:49 -0400

    testing

--- ui.js
+++ ui.js
@@ -1,6 +1,7 @@
-//Initial text fade-in
-$(function() {
-  $('.fade-in').each(function() {
-    $(this).fadeIn(1000).delay(1000);
+$(document).ready(function(){//Initial text fade-in
+  (function() {
+    $('.fade-in').each(function() {
+      $(this).fadeIn(1000).delay(1000);
+    });
   });
 });
