653a9f7b74771035c0af016a49664cdc93bab191
Jason Jalufka <jasonjalufka@gmail.com>
2016-08-30 13:12:01 -0500

    removed js alert and updated html and css

--- ui.js
+++ ui.js
@@ -1,8 +1,3 @@
 $(document).ready(function(){
-    alert("Hello! I am an alert box!");
     
-    //Initial text fade-in
-    $('.fade-in').each(function() {
-    $(this).fadeIn(1000).delay(1000);
-    });
 });
