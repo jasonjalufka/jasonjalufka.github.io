fad5a064da8aa76519f9aa7916b8a3e3cb5b298c
Jason Jalufka <jrj105@txstate.edu>
2017-07-08 01:12:36 -0500

    Make nicer

--- ui.js
+++ ui.js
@@ -2,15 +2,25 @@ $(document).ready(function() {
   
   console.log( "ready!" );
   
+  $("#getLocation").click(function() {
+    $.get("http://ipinfo.io", function (response) {
+      $("#getLocation").html("Location: " + response.city + ", " + response.region);
+    }, "jsonp").done(function() {
+      $("#getLocation").css("background-color", "#2ecc71");
+      $("#getLocation").css("color", "white");
+    })
+  });
+  
+  
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     
     if (scroll >= 30) {
-      $("nav").fadeOut();
+      $("nav").fadeOut(200);
     }
     else {
       $("nav").fadeIn();
     }
   });
-  
 });
+  
