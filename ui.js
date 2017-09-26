ae929262f3b08e839861743b7c8b53e3d5085a77
Jason Jalufka <jrj105@txstate.edu>
2017-09-26 13:45:13 -0500

    Update content on index and shuttleme

--- ui.js
+++ ui.js
@@ -2,14 +2,26 @@ $(document).ready(function() {
   
   console.log( "ready!" );
   
-  $("#getLocation").click(function() {
-    $.get("https://ipinfo.io", function (response) {
-      $("#getLocation").html("Location: " + response.city + ", " + response.region);
-    }, "jsonp").done(function() {
-      $("#getLocation").css("background-color", "#2ecc71");
-      $("#getLocation").css("color", "white");
-    })
+//  $("#getLocation").click(function() {
+//    $.get("https://ipinfo.io", function (response) {
+//      $("#getLocation").html("Location: " + response.city + ", " + response.region);
+//    }, "jsonp").done(function() {
+//      $("#getLocation").css("background-color", "#2ecc71");
+//      $("#getLocation").css("color", "white");
+//    })
+//  });
+  
+  $(window).onload = function() {
+  Particles.init({
+    selector: '.particle-background',
+      maxParticles: 100,
+      sizeVariations: 3,
+      speed: 0.5,
+      color: "#222222",
+      minDistance: 120,
+      connectParticles: true
   });
+};
   
   
   $(window).scroll(function() {
@@ -23,4 +35,3 @@ $(document).ready(function() {
     }
   });
 });
-  
